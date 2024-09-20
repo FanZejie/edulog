baaf2274f52501c74f135837f30ef72ec8521fa0
FanZejie <61533673+FanZejie@users.noreply.github.com>
2024-09-20 15:51:48 +0800

    fix: sidebarlogo can't load

--- src/components/dashboard/mySideBar.tsx
+++ src/components/dashboard/mySideBar.tsx
@@ -99,7 +99,7 @@ export const Logo = () => {
       href="/"
       className="font-normal flex space-x-2 items-center text-sm text-black pl-8 py-1 relative z-20">
       {/* <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" /> */}
-      <Image src="/sidebarlogo.png" alt="logo" width={20} height={20} />
+      <Image src="/sidebarLogo.png" alt="logo" width={20} height={20} />
       <motion.span
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
