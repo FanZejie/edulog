b1482b15dbc082afe4a8c2d54db6e40f755f3d2c
Fanzejie <fanzejiea@gmail.com>
2024-10-23 17:55:00 +0800

    feat:complete integration getmistake book

--- src/components/mistakebook/questionCollapse.tsx
+++ src/components/mistakebook/questionCollapse.tsx
@@ -1,51 +1,74 @@
 "use client";
 import { Collapse } from "antd";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { CollapseProps } from "antd";
 import Link from "next/link";
 
-const data = [
-  {
-    date: "2024-09-01",
-    practises: [
-      "unit1",
-      "unit2"
-    ],
-  },
-  {
-    date: "2024-09-02",
-    practises: [
-        "unit1",
-        "unit2"
-      ],
-  },
-];
 
-const formattedItems = data.map(
-  (item: { date: string; practises: Array<string> }) => ({
-    key: item.date, // 以日期作为 key
-    label: item.date, // 面板的标题为日期
-    children: (
-      <div className="flex flex-col gap-2">
-        {item.practises.map((practise,idx) => (
-          <Link key={idx} href={`/dashboard/mistake-book/details?date=${item.date}&practise=${practise}`} className="pl-4">
-            {practise}
-          </Link>
-        ))}
-      </div>
-    ),
-  })
-);
+interface DataItem {
+  date: string;
+  units: string[];
+}
+
+
 const WrongQuestionCollapse = () => {
-  const [items, setItems] = useState<CollapseProps["items"]>(formattedItems);
+  const [items, setItems] = useState<CollapseProps["items"]>([]);
   // 用来存储第一个日期的 key
-  const [defaultKey, setDefaultKey] = useState<string>(formattedItems[0].key);
+  const [defaultKey, setDefaultKey] = useState<string>('');
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        // 你的异步操作
+        const response = await fetch(`/api/getMistakeList`);
+        if (response.ok) {
+          const data = await response.json();
+          if (data.code == 0) {
+            setItems(data.data)
+            // 遍历 data 并对 units 去重
+            const uniqueData: DataItem[] = data.data.map((item: DataItem) => ({
+              date: item.date,
+              units: Array.from(new Set(item.units)), // 使用 Array.from
+            }));
 
+            const formattedItems = uniqueData.map(
+              (item: { date: string; units: Array<string> }) => ({
+                key: item.date, // 以日期作为 key
+                label: item.date, // 面板的标题为日期
+                children: (
+                  <div className="flex flex-col gap-2">
+                    {item.units.map((unit, idx) => (
+                      <Link key={idx} href={`/dashboard/vocabulary/details?date=${item.date}&unit=${unit}`} className="pl-4">
+                        {unit}
+                      </Link>
+                    ))}
+                  </div>
+                ),
+              })
+            );
+            setItems(formattedItems)
+            setDefaultKey(formattedItems[0]?.key || ''); // 设置第一个 key
+          } else {
+            console.error("code1:", data.message);
+          }
+        } else {
+          console.error("提交失败");
+          return null
+        }
+      };
+      fetchData();
+    } catch (error) {
+      console.error("请求出错:", error);
+    }
+  }, [])
   return (
-    <Collapse
-      items={items}
-      defaultActiveKey={defaultKey} // 设置默认展开的key
-    />
+    <>
+      {items && items.length > 0 && (
+        <Collapse
+          items={items}
+          defaultActiveKey={defaultKey} // 设置默认展开的key
+        />
+      )}
+    </>
   );
 };
 
