9642fe340bd85e1afb41f7c6bb06ad77e26b11f4
Fanzejie <fanzejiea@gmail.com>
2024-10-23 09:46:28 +0800

    feat:complete integration of Remove vocabulary

--- src/app/dashboard/vocabulary/details/page.tsx
+++ src/app/dashboard/vocabulary/details/page.tsx
@@ -2,6 +2,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+import { User } from "@/lib/type";
+import { message } from 'antd';
 
 interface DetailPageProps {
   params: Record<string, string | undefined>;
@@ -12,8 +15,59 @@ const Explanation = `An adventure is when you go on a fun and exciting journey, 
 const Example = `Lily went on an adventure with her family to the beach, where they found seashells and built a big sandcastle.`;
 const DetailPage: React.FC<DetailPageProps> = ({ params, searchParams }) => {
   const router = useRouter();
+
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const storedUser = localStorage.getItem('user');
+      if (storedUser) {
+        setUser(JSON.parse(storedUser));
+      }
+    }
+  }, []);
+
+  const [messageApi, contextHolder] = message.useMessage();
+  const success = () => {
+    messageApi.open({
+      type: 'success',
+      content: 'remove success',
+    });
+  };
+
+  const removeWord = async ()=>{
+    // 发送数据到后端
+    const userName = user?.userName
+    const word = searchParams.practise
+    const date = searchParams.date
+    try {
+      const response = await fetch('/api/deleteVocabularyWord', {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({userName,word,date}),
+      });
+
+      if (!response.ok) {
+        throw new Error('网络错误');
+      }
+
+      // 处理后端响应
+      const result = await response.json();
+      console.log('result',result)
+      if(result.code == 0){
+        success()
+      }else{
+        console.error('后端返回:', result.message);
+      }
+      
+    } catch (error) {
+      console.error('发送失败:', error);
+    }
+  }
   return (
     <div className="flex flex-col mt-12">
+      {contextHolder}
       <div className="flex flex-row w-2/3 justify-between">
         <h1 className="text-4xl font-bold">Vocabulary</h1>
         <Link
@@ -35,7 +89,7 @@ const DetailPage: React.FC<DetailPageProps> = ({ params, searchParams }) => {
       <div className="flex flex-row justify-center mt-12 w-2/3">
         <button
           onClick={() => {
-            router.push("/dashboard/mistake-book");
+            removeWord()
           }}
           className="bg-[#EF6767] text-white font-bold py-2 w-[364px] flex items-center justify-center rounded  ">
           Remove Word
